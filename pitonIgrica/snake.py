fee33c4ddc04ee66d7070235ce1a3468ba60b630
Milk696969 <105432543+Milk696969@users.noreply.github.com>
2022-05-14 21:25:24 +0200

    Add files via upload

---
+++ pitonIgrica/snake.py
@@ -0,0 +1,35 @@
+import pygame 
+from pygame.locals import * 
+
+hight  = int(input("Enter hight: "))
+width  = int(input("Enter width: "))
+
+pokretanje = True
+
+#sve vezano za igraca
+
+
+while pokretanje == True:
+
+    
+    #display
+    window = pygame.display.set_mode((hight, width))
+
+    window.fill((255, 0, 0))
+
+
+    #igrac
+    igrac = pygame.image.load("ase48.png").convert()
+    window.blit(igrac, (hight/2 - 32, width/2 - 32))
+    
+    pygame.display.flip()
+
+
+    for event in pygame.event.get():
+
+        #provjerava koja je tipka pritisnuta
+        if(event.type == KEYDOWN):
+
+            #ako je ovo pritisnuto zaustavlja igru
+            if(event.key == K_ESCAPE):
+                pokretanje = False
