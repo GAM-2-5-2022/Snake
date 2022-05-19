468ecba0e7e65a6554d414bf17214e8f929ef15f
Milk696969 <105432543+Milk696969@users.noreply.github.com>
2022-05-19 17:22:47 +0200

    Update snake.py

--- pitonIgrica/snake.py
+++ pitonIgrica/snake.py
@@ -1,4 +1,6 @@
-import pygame 
+from math import fabs
+from tkinter import EventType
+import pygame
 from pygame.locals import * 
 
 hight  = int(input("Enter hight: "))
@@ -8,6 +10,14 @@ pokretanje = True
 
 #sve vezano za igraca
 
+x = hight/2 - 32
+y = width/2 - 32
+
+
+naprijed = False
+dolje = False
+desno = False
+lijevo = False
 
 while pokretanje == True:
 
@@ -20,15 +30,53 @@ while pokretanje == True:
 
     #igrac
     igrac = pygame.image.load("ase48.png").convert()
-    window.blit(igrac, (hight/2 - 32, width/2 - 32))
+    window.blit(igrac, (x, y))
     
     pygame.display.flip()
+    
+    #micanje igraca
+    if(naprijed):
+        y -= 0.1
+    
+    elif(dolje):
+        y += 0.1
+
+    elif(desno):
+        x += 0.1
+
+    elif(lijevo):
+        x -= 0.1
 
 
     for event in pygame.event.get():
 
         #provjerava koja je tipka pritisnuta
         if(event.type == KEYDOWN):
+
+            if(event.key == K_w):
+                naprijed = True
+                dolje = False
+                desno = False
+                lijevo = False
+
+            elif(event.key == K_s):
+                naprijed = False
+                dolje = True
+                desno = False
+                lijevo = False
+
+            elif(event.key == K_d):
+                naprijed = False
+                dolje = False
+                desno = True
+                lijevo = False
+
+            elif(event.key == K_a):
+                naprijed = False
+                dolje = False
+                desno = False
+                lijevo = True
+
 
             #ako je ovo pritisnuto zaustavlja igru
             if(event.key == K_ESCAPE):
